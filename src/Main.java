cac37107f5be7a275096c5cba23fb757945da225
Sakura <sakurathetrans@gmail.com>
2025-04-03 23:34:31 -0400

    code :3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner input = new Scanner(System.in);
+        while(true) {
+
+            System.out.println("First Number?");
+            int num1 = input.nextInt();
+            input.nextLine();
+
+            System.out.println("Second Number?");
+            int num2 = input.nextInt();
+            input.nextLine();
+
+            if (num1 % 10 == num2 % 10) {
+                System.out.println("Last Digits Are The Same!");
+            } else {
+                System.out.println("Last Digits Are NOT The Same!");
+            }
+
+            System.out.println("Do you wish to exit? (Y/N)");
+            String exitInput = input.nextLine();
+            if (exitInput.equalsIgnoreCase("y")) {
+                return;
+            }
+
+        }
     }
 }
